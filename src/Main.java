ecb3834acbafb2c2845b4d01e249b9d792a99fd1
sameer979 <behna9797@gmail.com>
2022-10-15 23:11:54 +0530

    Linked List Done Singly Linked List Only

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+      LinkedList list = new LinkedList();
+      list.insertFirst(1);
+      list.insertFirst(5);
+      list.insertFirst(8);
+      list.insertFirst(10);;
+
+      list.insertLast(55);
+      list.insertLast(89);
+      list.insertLast(69);
+      list.insert(50,5);
+      list.insert(75,2);
+      list.display();
+
+      System.out.println(list.delete(5));
+      list.display();
+    }
+
+
+}
